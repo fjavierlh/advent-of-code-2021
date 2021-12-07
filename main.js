2ad89a3f92a4eed7ce92d2b7c06668b33eeac503
Javier López <fjavierlh@gmail.com>
2021-12-07 18:26:39 +0100

    feat: added to main file day 1 result

---
+++ main.js
@@ -0,0 +1,13 @@
+import fetch from "node-fetch";
+import { getNumberMeasurementsIncrease, getSumByThreeMeasurementsIncrease } from "./day-01-sonar-sweep/index.js";
+import { inputDataDay01 } from "./day-01-sonar-sweep/input.data.js";
+import log from "./logger.js";
+
+log("Advent of code 2021");
+
+log("--- Day 1: Sonar Sweep ---");
+const resultDay01Part1 = getNumberMeasurementsIncrease(inputDataDay01);
+log(`Result part 1: ${resultDay01Part1}`);
+const measurementsByThree = getSumByThreeMeasurementsIncrease(inputDataDay01);
+const resultDay01Part2 = getNumberMeasurementsIncrease(measurementsByThree);
+log(`Result part 2: ${resultDay01Part2}`)
