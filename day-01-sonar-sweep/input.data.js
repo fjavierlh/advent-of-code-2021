0ddd7404e6b0fd098804797381373ba917e7889c
Javier López <fjavierlh@gmail.com>
2021-12-08 01:50:11 +0100

    refactor: removed unused import

--- day-01-sonar-sweep/input.data.js
+++ day-01-sonar-sweep/input.data.js
@@ -1,5 +1,4 @@
 import * as fs from "fs";
-import * as path from 'path';
 
 const inputRawData = fs.readFileSync("./day-01-sonar-sweep/input.txt", "utf8");
 export const inputDataDay01 = inputRawData.split(/\n/).map((m) => +m);
--- day-02-dive/input.data.js
+++ day-02-dive/input.data.js
@@ -1,5 +1,4 @@
 import * as fs from "fs";
-import * as path from 'path';
 
 const inputRawData = fs.readFileSync("./day-02-dive/input.txt", "utf8");
 export const inputDataDay02 = inputRawData.split(/\n/).map((command) => {
