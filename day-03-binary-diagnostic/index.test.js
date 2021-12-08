3fedf13ff740f4a62402fa5535ea444b68f776f6
Javier López <fjavierlh@gmail.com>
2021-12-08 12:33:39 +0100

    test: updated day 3 tests

--- day-03-binary-diagnostic/index.test.js
+++ day-03-binary-diagnostic/index.test.js
@@ -1,4 +1,4 @@
-import { getSubmarinePowerConsumption } from ".";
+import { getSubmarineLifeSupportRating, getSubmarinePowerConsumption } from ".";
 
 describe("getSubmarinePowerConsumption", () => {
   const mockInput = [
@@ -17,9 +17,9 @@ describe("getSubmarineLifeSupportRating", () => {
     "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010",
     "01010",
   ];
-  test("should return the power consumption of the submarine ", () => {
+  test("should return the life support rating of the submarine ", () => {
     const expectedResult = 230;
-    const result = getSubmarinePowerConsumption(mockInput);
+    const result = getSubmarineLifeSupportRating(mockInput);
     expect(result).toBe(expectedResult);
   });
 });
